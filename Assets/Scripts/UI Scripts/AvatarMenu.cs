db10b1d0ae5b2d8de816415265e803611e630298
jackmok33 <jackmok33@gmail.com>
2024-12-05 17:56:55 +0000

    created connection from ui to ar scene

--- Assets/Scripts/UI Scripts/AvatarMenu.cs
+++ Assets/Scripts/UI Scripts/AvatarMenu.cs
@@ -6,7 +6,7 @@ public class AvatarMenu : MonoBehaviour
 {
     public TMP_Text userNameDisplay; // Reference to TMP_Text (used to display the username)
 
-    void Start()
+    public void Start()
     {
         // Retrieve the saved username from PlayerPrefs
         string userName = PlayerPrefs.GetString("UserName", "Guest"); // Default to "Guest" if not found
@@ -15,4 +15,10 @@ public class AvatarMenu : MonoBehaviour
         userNameDisplay.text = "Welcome, " + userName + "!";
         Debug.Log("Displayed username: " + userName);
     }
+    public void OnSubmit()
+    {
+        // Transition to AR scene with a board
+        SceneManager.LoadScene(2);
+    }
+
 }
