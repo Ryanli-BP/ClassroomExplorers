b1bffebd023a865b3c8999b337961216b072868d
jackmok33 <jackmok33@gmail.com>
2025-02-08 14:49:21 +0000

    changed only one player prefab for all

--- Assets/CharacterManager.cs
+++ Assets/CharacterManager.cs
@@ -3,39 +3,112 @@ using UnityEngine.SceneManagement;
 
 public class CharacterManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField] private GameObject[] characters;
-    private int selectedCharacterIndex = 0;
+    [SerializeField] private GameObject player;
 
-    public void NextCharacter()
+    private GameObject[] bodyColors;
+    private int selectedBodyColorIndex = 0;
+
+    private GameObject[] hats;
+    private int selectedHatIndex = 0;
+
+    private void Start()
     {
-        characters[selectedCharacterIndex].SetActive(false);
-        selectedCharacterIndex = (selectedCharacterIndex + 1) % characters.Length;
-        characters[selectedCharacterIndex].SetActive(true);
+        InitializeBodyColors();
+        InitializeHats();
+
+        // Show the first body color and hat by default
+        if (bodyColors.Length > 0)
+        {
+            bodyColors[selectedBodyColorIndex].SetActive(true);
+        }
+        if (hats.Length > 0)
+        {
+            hats[selectedHatIndex].SetActive(true);
+        }
     }
 
-    public void PreviousCharacter()
+    private void InitializeBodyColors()
     {
-        characters[selectedCharacterIndex].SetActive(false);
-        selectedCharacterIndex = (selectedCharacterIndex - 1 + characters.Length) % characters.Length;
-        characters[selectedCharacterIndex].SetActive(true);
+        Transform bodyParent = player.transform.Find("Mesh Object/Bone_Body");
+        if (bodyParent != null)
+        {
+            int colorCount = bodyParent.childCount;
+            bodyColors = new GameObject[colorCount];
+
+            for (int i = 0; i < colorCount; i++)
+            {
+                bodyColors[i] = bodyParent.GetChild(i).gameObject;
+                bodyColors[i].SetActive(false); // Deactivate all body colors initially
+            }
+        }
+        else
+        {
+            bodyColors = new GameObject[0];
+        }
+    }
+
+    private void InitializeHats()
+    {
+        Transform hatsParent = player.transform.Find("hats");
+        if (hatsParent != null)
+        {
+            int hatCount = hatsParent.childCount;
+            hats = new GameObject[hatCount];
+
+            for (int i = 0; i < hatCount; i++)
+            {
+                hats[i] = hatsParent.GetChild(i).gameObject;
+                hats[i].SetActive(false); // Deactivate all hats initially
+            }
+        }
+        else
+        {
+            hats = new GameObject[0];
+        }
+    }
+
+    public void NextBodyColor()
+    {
+        if (bodyColors.Length == 0) return;
+
+        bodyColors[selectedBodyColorIndex].SetActive(false);
+        selectedBodyColorIndex = (selectedBodyColorIndex + 1) % bodyColors.Length;
+        bodyColors[selectedBodyColorIndex].SetActive(true);
+    }
+
+    public void PreviousBodyColor()
+    {
+        if (bodyColors.Length == 0) return;
+
+        bodyColors[selectedBodyColorIndex].SetActive(false);
+        selectedBodyColorIndex = (selectedBodyColorIndex - 1 + bodyColors.Length) % bodyColors.Length;
+        bodyColors[selectedBodyColorIndex].SetActive(true);
+    }
+
+    public void NextHat()
+    {
+        if (hats.Length == 0) return;
+
+        hats[selectedHatIndex].SetActive(false);
+        selectedHatIndex = (selectedHatIndex + 1) % hats.Length;
+        hats[selectedHatIndex].SetActive(true);
+    }
+
+    public void PreviousHat()
+    {
+        if (hats.Length == 0) return;
+
+        hats[selectedHatIndex].SetActive(false);
+        selectedHatIndex = (selectedHatIndex - 1 + hats.Length) % hats.Length;
+        hats[selectedHatIndex].SetActive(true);
     }
 
     public void StartGame()
     {
-        // Save the selected character's index to PlayerPrefs
-        PlayerPrefs.SetInt("SelectedCharacterIndex", selectedCharacterIndex);
+        PlayerPrefs.SetInt("SelectedBodyColorIndex", selectedBodyColorIndex);
+        PlayerPrefs.SetInt("SelectedHatIndex", selectedHatIndex);
         PlayerPrefs.Save();
 
-        // Load the next scene
         SceneManager.LoadScene("AR Board Scene");
-    }
-
-    private void Start()
-    {
-        if (characters.Length > 0)
-        {
-            characters[selectedCharacterIndex].SetActive(true); // Initialize character visibility
-        }
     }
 }
