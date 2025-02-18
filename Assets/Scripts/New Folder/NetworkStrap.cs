66c2307224f8bd7d7df2f643a2231e81a082079b
Pran-bho <prantanilbhowmick@gmail.com>
2025-02-18 18:30:00 +0000

    Photon Import  + Network Strapper

---
+++ Assets/Scripts/New Folder/NetworkStrap.cs
@@ -0,0 +1,61 @@
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NetworkStrap : MonoBehaviourPunCallbacks
+{
+    public static NetworkStrap Instance;
+    public const string GAME_SCENE = "AR Board Scene";
+    public const string LOBBY_SCENE = "Lobby";
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); 
+        }
+        else
+        {
+            Debug.LogWarning("Duplicate NetworkStrapper detected. Destroying this instance.");
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        if (!PhotonNetwork.IsConnected)
+        {
+            ConnectToPhoton();
+            
+        }
+    }
+
+    private void ConnectToPhoton()
+    {
+        Debug.Log("Connecting to Photon...");
+        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master!");
+        if (!PhotonNetwork.InLobby)
+        {
+            PhotonNetwork.JoinLobby();
+        }
+    }
+
+    public void LoadLobbyScene()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.LoadLevel(LOBBY_SCENE);
+        }
+        else
+        {
+            Debug.LogWarning("Not connected to Photon! Cannot load lobby.");
+        }
+    }
+}
